d69f56a9350876102954cc9c1e5ad0a4e575ec06
Wojciech Bal <wojciech.bal@epoczta.pl>
2024-09-14 13:25:41 +0200

    Add more fancy controller tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,20 +1,38 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import * as request from 'supertest';
 
 describe('AppController', () => {
   let appController: AppController;
+  let app: INestApplication;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = module.get<AppController>(AppController);
+
+    app = module.createNestApplication();
+    await app.init();
   });
 
   describe('root', () => {
     it('should return web property with ok value', () => {
       expect(appController.health()).toEqual({ web: 'ok' });
+    });
+
+    it('should return HTTP 200 status', () => {
+      return request(app.getHttpServer()).get('/').expect(200);
+    });
+
+    it('should return json response', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .then((response) => {
+          expect(response.body).toEqual({ web: 'ok' });
+        });
     });
   });
 });
